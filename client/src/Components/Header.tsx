206ccf3a8786e34e1ac559e578736a5042c34039
Erkan <a22eriwi@student.his.se>
2025-08-27 19:39:29 +0200

    Continued with the header
    Continued styling the header especially the nav meny

--- client/src/Components/Header.tsx
+++ client/src/Components/Header.tsx
@@ -1,6 +1,8 @@
 import { useTheme } from "../useTheme";
 import DarkIcon from "../assets/darkMode.svg?react";
 import LightIcon from "../assets/lightMode.svg?react";
+import KontoIcon from "../assets/konto.svg?react";
+import Logga from "../assets/logga.svg?react";
 import { useState } from 'react'
 import {
   Dialog,
@@ -22,23 +24,13 @@ export default function Header() {
 
   return (
     <header>
-      <nav aria-label="Global" className="flex items-center justify-between lg:px-8 max-w-full p-6 pb-0 ">
+      <nav aria-label="Global" className="flex items-center justify-between sm:px-8 max-w-full px-4 pb-0 pt-0">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="loggaSmall.png"
-              className="h-10 w-auto sm:hidden"
-            />
-            <img
-              alt=""
-              src="loggaBig.png"
-              className="h-12 w-auto hidden sm:block"
-            />
+          <a href="#">
+            <Logga/>
           </a>
         </div>
-        <div className="flex lg:hidden">
+        <div className="flex lg:hidden items-center">
           <button
             onClick={() => setTheme(isDark ? "light" : "dark")}
             className="relative flex h-8 w-18 items-center rounded group"
@@ -47,20 +39,23 @@ export default function Header() {
               className={`absolute left-1 h-8 w-9 rounded-full inset-shadow-sm inset-shadow-indigo-400 duration-400 ease-in-out transform transition-transform ${isDark ? "translate-x-7.5" : "-translate-x-0.5"
                 }`}
             />
-            <LightIcon className="absolute left-2  h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200"/>
-            <DarkIcon className="absolute right-2 h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200"/>
+            <LightIcon className="absolute left-2  h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200" />
+            <DarkIcon className="absolute right-2 h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200" />
+          </button>
+          <button className=" p-2">
+            <KontoIcon className="h-6 w-6  hover:text-sky-800 dark:hover:text-gray-200" />
           </button>
           {/* Hamburger menu */}
           <button
             type="button"
             onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 ml-1 inline-flex items-center justify-center rounded-md p-2.5"
+            className="inline-flex justify-center rounded-md p-2 hover:text-sky-800 dark:hover:text-gray-200"
           >
             <span className="sr-only">Open main menu</span>
             <Bars3Icon aria-hidden="true" className="size-6" />
           </button>
         </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+        <PopoverGroup className="hidden lg:flex">
           <Popover className="relative">
             <PopoverPanel
               transition
@@ -68,15 +63,26 @@ export default function Header() {
             >
             </PopoverPanel>
           </Popover>
-          <a href="#" className="text-base/6 font-semibold">
-            Art
-          </a>
-          <a href="#" className="text-base/6 font-semibold">
-            Artists
-          </a>
-          <a href="#" className="text-base/6 font-semibold">
-            About
-          </a>
+          <div className="lg:flex lg:gap-x-10">
+            <a
+              href="#"
+              className="relative text-base/6 font-semibold after:absolute after:left-0 after:translate-y-[15px] after:bottom-0 after:h-[2px] after:w-0 after:bg-gray-800 dark:after:bg-gray-200 after:transition-all after:duration-250 after:ease-in-out hover:after:w-full">
+              Paintings
+            </a>
+            <a
+              href="#"
+              className="relative text-base/6 font-semibold after:absolute after:left-0 after:translate-y-[15px] after:bottom-0 after:h-[2px] after:w-0 after:bg-gray-800 dark:after:bg-gray-200 after:transition-all after:duration-250 after:ease-in-out hover:after:w-full">
+              Photography
+            </a>
+            <a href="#"
+              className="relative text-base/6 font-semibold after:absolute after:left-0 after:translate-y-[15px] after:bottom-0 after:h-[2px] after:w-0 after:bg-gray-800 dark:after:bg-gray-200 after:transition-all after:duration-250 after:ease-in-out hover:after:w-full">
+              Artists
+            </a>
+            <a href="#"
+              className="relative text-base/6 font-semibold after:absolute after:left-0 after:translate-y-[15px] after:bottom-0 after:h-[2px] after:w-0 after:bg-gray-800 dark:after:bg-gray-200 after:transition-all after:duration-250 after:ease-in-out hover:after:w-full">
+              About
+            </a>
+          </div>
         </PopoverGroup>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center">
           <button
@@ -87,17 +93,17 @@ export default function Header() {
               className={`absolute left-1 h-8 w-9 rounded-full inset-shadow-sm inset-shadow-indigo-400 duration-400 ease-in-out transform transition-transform ${isDark ? "translate-x-7.5" : "-translate-x-0.5"
                 }`}
             />
-            <LightIcon className="absolute left-2  h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200"/>
-            <DarkIcon className="absolute right-2 h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200"/>
+            <LightIcon className="absolute left-2  h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200" />
+            <DarkIcon className="absolute right-2 h-6 w-6 group-hover:text-sky-800 dark:group-hover:text-gray-200" />
           </button>
-          <a href="#" className="text-base/6 font-semibold ml-4 hover:text-sky-800 hover:dark:text-gray-200">
-            Log in
-          </a>
+          <button className=" p-2">
+            <KontoIcon className="h-6 w-6 hover:text-sky-800 dark:hover:text-gray-200" />
+          </button>
         </div>
       </nav>
       {/* Menu open*/}
       <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-40 bg-black/40 transition-opacity duration-100 data-[closed]:opacity-0"/>
+        <div className="fixed inset-0 z-40 bg-black/40 transition-opacity duration-100 data-[closed]:opacity-0" />
         <DialogPanel
           transition
           className="fixed inset-y-0 right-0 z-50 w-3/4 sm:w-1/2 bg-gray-200 dark:bg-gray-900 dark:text-gray-300 p-6 pt-9 sm:pt-9 overflow-y-auto h-screen transform transition-transform duration-400 ease-in-out data-[closed]:translate-x-full">
@@ -109,14 +115,14 @@ export default function Header() {
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
-                className="-m-2.5 ml-1 rounded-md p-2.5"
+                className="-m-2.5 ml-1 rounded-md p-2.5 hover:text-sky-800 dark:hover:text-gray-200"
               >
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon aria-hidden="true" className="size-6" />
               </button>
             </div>
           </div>
-          <div className="mt-6 flow-root ">
+          <div className="mt-6 flow-root text-center">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 <a
@@ -136,14 +142,6 @@ export default function Header() {
                   className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold hover:bg-gray-100 hover:dark:bg-gray-800 hover:dark:text-gray-100"
                 >
                   About
-                </a>
-              </div>
-              <div className="py-6">
-                <a
-                  href="#"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base/7 font-semibold hover:bg-gray-100 hover:dark:bg-gray-800 hover:dark:text-gray-100"
-                >
-                  Log in
                 </a>
               </div>
             </div>
