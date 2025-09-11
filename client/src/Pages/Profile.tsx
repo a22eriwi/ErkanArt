c4946fc07593b67c1f4abcef25dc32c0f426d408
Erkan <a22eriwi@student.his.se>
2025-09-11 16:07:23 +0200

    Styling update on Profile.tsx

--- client/src/Pages/Profile.tsx
+++ client/src/Pages/Profile.tsx
@@ -18,11 +18,11 @@ export default function Profile() {
 
   return (
     <div>
-      <div className="lg:max-w-[900px] xl:max-w-[1100px] m-auto px-5">
-        <h1 className="text-2xl font-semibold mt-10 text-center"> {user?.firstName} {user?.lastName}</h1>
+      <div className="lg:max-w-[900px] xl:max-w-[1100px] m-auto px-5 mt-10">
+        <h1 className="text-2xl font-semibold text-center"> {user?.firstName} {user?.lastName}</h1>
 
         {/* Tabs */}
-        <div className="flex gap-2 justify-center mt-5">
+        <div className="flex gap-0 sm:gap-2 justify-center mt-5">
           <button onClick={() => setActiveTab("favorites")} className={`px-4 py-3 text-sm font-semibold rounded-md
           ${activeTab === "favorites" ? "underline decoration-2 underline-offset-10" : "dark:hover:bg-gray-800 hover:bg-gray-200/70"}`}>
             Favorites
@@ -39,7 +39,7 @@ export default function Profile() {
           </button>
         </div>
 
-        <div className="mt-10 text-center">
+        <div className="mt-8 text-center">
           {activeTab === "favorites" &&
             <div>
               <p>Favorites</p>
