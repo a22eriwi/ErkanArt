5d826391c4520da4b4f6efe838f260fb0f966fb7
Erkan <a22eriwi@student.his.se>
2025-09-20 21:48:21 +0200

    updated imports

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,16 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.tsx';
 import './index.css';
 
-import About from './Pages/About.tsx';
-import Artists from './Pages/Artists.tsx';
-import Home from './Pages/Home.tsx';
-import Paintings from './Pages/Paintings.tsx';
-import Photography from "./Pages/Photography.tsx";
-import Profile from "./Pages/Profile/Profile.tsx";
+import About from './pages/about.tsx';
+import Artists from './pages/artists.tsx';
+import Home from './pages/home.tsx';
+import Paintings from './pages/paintings.tsx';
+import Photography from "./pages/photography.tsx";
+import Profile from "./pages/profile/profile.tsx";
 
-import Favorites from "./Pages/Profile/Favorites.tsx";
-import MyPaintings from "./Pages/Profile/MyPaintings.tsx";
-import MyPhotographs from "./Pages/Profile/MyPhotographs.tsx";
+import Favorites from "./pages/profile/favorites.tsx";
+import MyPaintings from "./pages/profile/myPaintings.tsx";
+import MyPhotographs from "./pages/profile/myPhotographs.tsx";
 
 const router = createBrowserRouter([
   {
