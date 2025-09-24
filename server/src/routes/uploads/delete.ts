3428637419a62fa75addc15aa7a02a6755f38014
Erkan <a22eriwi@student.his.se>
2025-09-24 19:25:49 +0200

    Import updated

--- server/src/routes/uploads/delete.ts
+++ server/src/routes/uploads/delete.ts
@@ -1,5 +1,5 @@
 // server/src/routes/uploads/delete.ts
-import Upload from "../../models/Upload";
+import Upload from "../../models/upload";
 import { Router } from "express";
 import { requireAuth, requireApproved, AuthRequest } from "../../middleware/auth";
 import { r2 } from "../../r2";
