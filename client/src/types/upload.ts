143f781c1361da8d4f2d8667356b98e52b3e1ecb
Erkan <a22eriwi@student.his.se>
2025-09-24 19:16:30 +0200

    Added upload.ts
    New centralized type for Uploads

---
+++ client/src/types/upload.ts
@@ -0,0 +1,17 @@
+// client/src/types/upload.ts
+export type Upload = {
+  _id: string;
+  title: string;
+  description?: string;
+  type: "painting" | "photograph";
+  url: string;
+  sizes?: {
+    thumbnail?: string | null;
+    medium?: string | null;
+  };
+  owner: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  };
+};
