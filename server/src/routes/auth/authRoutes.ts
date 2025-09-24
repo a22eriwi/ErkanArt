18ec6617adf29b8741af1b56f60f8514bb2ce034
Erkan <a22eriwi@student.his.se>
2025-09-24 19:29:16 +0200

    Moved authRoutes.ts into the new auth folder

--- server/src/routes/auth/authRoutes.ts
+++ server/src/routes/auth/authRoutes.ts
@@ -2,7 +2,7 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User";
+import User from "../../models/user";
 
 const router = Router();
 
