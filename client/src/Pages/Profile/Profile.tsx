d52fada214bde5a6acf981694a6645b755adc745
Erkan <a22eriwi@student.his.se>
2025-09-29 17:41:44 +0200

    Updated responsive styling

--- client/src/Pages/Profile/Profile.tsx
+++ client/src/Pages/Profile/Profile.tsx
@@ -28,7 +28,7 @@ export default function Profile() {
   }
 
   return (
-    <div className="lg:max-w-[900px] xl:max-w-[1100px] m-auto px-5 mt-10">
+    <div className="lg:max-w-[90vw] 2xl:max-w-[80vw] 3xl:max-w-[65vw] m-auto px-5 mt-10">
       <Outlet context={{ openUpload, setOnUploadSuccess }} /> {/* Renders Favorites, MyPaintings, or MyPhotographs */}
 
       <UploadModal
