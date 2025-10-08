441b9084af0dc87467ff21f82adf8e3371950e08
Erkan <a22eriwi@student.his.se>
2025-10-08 17:51:22 +0200

    Add approval check and slideshow buttons to Favorites
    Favorites page now displays slideshow action buttons for approved users and shows a message for users awaiting approval. Added icons and integrated user approval logic using auth context.

--- client/src/Pages/Profile/Favorites.tsx
+++ client/src/Pages/Profile/Favorites.tsx
@@ -1,8 +1,33 @@
 // client/src/Pages/Profile/Favorites.tsx
+import { useAuth } from "../../components/authContext"
+import CreateSlideShowIcon from "../../assets/createSlideShowIcon.svg?react";
+import PlaySlideShow from "../../assets/playSlideShowIcon.svg?react";
+
 export default function Favorites() {
+    const { user } = useAuth();
+
     return (
-        <div className=" flex justify-center items-center">
-            <h1 className="font-semibold text-2xl">Favorites</h1>
+        <div>
+            {user?.isApproved ? (
+                <>
+                    <div className="flex justify-center items-center gap-3 mb-4">
+                        <h1 className="font-semibold text-2xl">Favorites</h1>
+
+                        <div className="flex gap-2">
+                            <button className="p-2 flex items-center gap-1 btn btn-secondary hover:">
+                                <CreateSlideShowIcon className="size-6" />
+                            </button>
+                            <button className="p-2 flex items-center gap-1 btn btn-secondary hover:">
+                                <PlaySlideShow className="size-6" />
+                            </button>
+                        </div>
+
+
+                    </div>
+                </>
+            ) : (
+                <p>Your account is awaiting approval. You cannot upload yet.</p>
+            )}
         </div>
-    )
+    );
 }
