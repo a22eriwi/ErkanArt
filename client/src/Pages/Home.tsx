12af2bc8108924acdb238f210eb380b0e8d36afb
Erkan <a22eriwi@student.his.se>
2025-10-08 13:39:44 +0200

    Update Home page layout and sections
    Refactored the Home page to include separate sections for recently added paintings and photographs, with placeholder text.

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,7 +1,18 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-center text-2xl font-semibold mb-5">Home</h1>
+    <div className="lg:max-w-[90vw] 2xl:max-w-[80vw] 3xl:max-w-[65vw] m-auto px-5">
+
+      {/* Recent Paintings */}
+      <div>
+        <h1 className="text-2xl font-semibold">Recently added paintings</h1>
+        <p>Coming soon!</p>
+      </div>
+
+      {/* Recent Photographs */}
+      <div className="mt-10">
+        <h1 className="text-2xl font-semibold">Recently added photographs</h1>
+        <p>Coming soon!</p>
+      </div>
     </div>
   )
 }
